f7a08f17054a7dc21791f2541b8936052ebd2289
mustaphaaliyu520 <musyaphaaliyu520@gmail.com>
2023-02-10 22:34:17 +0100

    first commit

---
+++ operators.js
@@ -0,0 +1,64 @@
+let num1 = 10;
+let num2 = 5;
+
+//Addition
+let sum = num1 + num2;
+console.log("Sum: ", sum);
+
+//subtraction
+let difference = num1 - num2;
+console.log("Difference: ", difference);
+
+
+//multiplication
+let product = num1 * num2;
+console.log("product: ", product);
+
+//division
+let result = num1 / num2;
+console.log("Result: ", result);
+
+//percentage
+let remainder = num1 % num2;
+console.log("Remainder: ", remainder);
+
+//increase num1 by 1
+num1++;
+console.log("num1 after increment: ", num1);
+
+//decrease num2 by 1
+num2--;
+console.log("num2 after decrement: ", num2);
+
+//prompt function
+let input1 = prompt("Enter the first number: ");
+let input2 = prompt("Enter the second number: ");
+
+//convert inputs to numbers
+input1 = parseFloat(input1);
+input2 = parseFloat(input2);
+
+console.log("First input(number): ", input1);
+console.log("Second input(number): ", input2);
+
+//mathematical operations based on user input
+
+//addition
+let addition = input1 + input2;
+console.log("Addition: ", addition);
+
+//subtraction
+let subtraction = input1 - input2;
+console.log("Subtraction: ", subtraction);
+
+//multiplacation
+let multiplication = input1 * input2;
+console.log("Multiplication: ", multiplication);
+
+//division
+let division = input1 / input2;
+console.log("division: ", division);
+
+//percentage
+let percentage = input1 % input2;
+console.log("percentage: ", percentage);
